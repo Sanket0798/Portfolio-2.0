bc3904ddfff1bbd725c6b0afe6c94cb37df46f4a
Sanket0798 <rathodsanket2022@gmail.com>
2024-11-03 19:53:44 +0530

    CSS files has been modified with some minor changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      textColor: {
+        h1: "#4db5ff", // Color for h1
+        h2: "#ffffff", // Color for h2
+        h3: "#ffffff", // Color for h3
+        h4: "#ffffff", // Color for h4
+        p: "#ffffff", // Color for paragraphs
+      },
     },
   },
   plugins: [],
